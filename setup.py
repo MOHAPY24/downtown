49b4f692ee29b4607845a661841ddf71fd4124dc
MOHAPY24 <mohammedmahmoud123b@gmail.com>
2025-11-01 17:21:16 +0400

    basic setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="downtown",
+    version="0.1.0",
+    description="A Python framework for making websites from Markdown and CSS files.",
+    license="GPLv3",
+    author="MOHAPY24",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.12",
+    ]
+)
