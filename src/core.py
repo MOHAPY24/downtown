98d8a50eedfce792d2c8bbb8c74ca4618693c721
MOHAPY24 <mohammedmahmoud123b@gmail.com>
2025-11-01 21:02:17 +0400

    .Site files for inidividual file customization

--- src/core.py
+++ src/core.py
@@ -11,28 +11,35 @@ from pathlib import Path
 def build():
 
     for i in sitefile["Files"]:
+        savesite = sitefile
+        if i != "Index":
+            try:
+                with open(f"build/configs/{i}.Site", 'r') as f:
+                    savesite = json.loads(utils.rmcomments(f.read()))
+            except FileNotFoundError:
+                pass
         print(f"[*] Making: '{sitefile["Files"][i]}'")
         soup = BeautifulSoup(template, "html.parser")
         body = soup.body
         with open(sitefile["Files"][i], 'r') as f:
             mark = f.read()
 
-        soup.title.string = sitefile["metadata"]["Title"]
+        soup.title.string = savesite["metadata"]["Title"]
         meta_desc = soup.find("meta", attrs={"name": "description"})
         stylesheet = soup.find("link", attrs={"rel": "stylesheet"})
 
         if stylesheet:
-            stylesheet["href"] = sitefile["metadata"]["Stylesheet-file"]
+            stylesheet["href"] = savesite["metadata"]["Stylesheet-file"]
         else:
-            new_link = soup.new_tag("link", rel="stylesheet", href=sitefile["metadata"]["Stylesheet-file"])
+            new_link = soup.new_tag("link", rel="stylesheet", href=savesite["metadata"]["Stylesheet-file"])
             soup.head.append(new_link)
 
         if meta_desc:
-            meta_desc["content"] = sitefile["metadata"]["Description"]
+            meta_desc["content"] = savesite["metadata"]["Description"]
         else:
             new_meta = soup.new_tag("meta")
             new_meta.attrs["name"] = "description"
-            new_meta.attrs["content"] = sitefile["metadata"]["Description"]
+            new_meta.attrs["content"] = savesite["metadata"]["Description"]
             soup.head.append(new_meta)
 
         body.append(BeautifulSoup(markdown.markdown(mark), "html.parser"))
